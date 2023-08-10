cf829eec4ede2b6d85bed9999a6524fdf5f2da68
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-08-10 22:53:49 +0200

    Implement book service validations tests

--- internal/services/book_service_test.go
+++ internal/services/book_service_test.go
@@ -1,6 +1,10 @@
 package services
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestGetBooks(t *testing.T) {
 	// TODO: Implement
@@ -23,13 +27,87 @@ func TestDeleteBook(t *testing.T) {
 }
 
 func TestValidateID(t *testing.T) {
-	// TODO: Implement
+	bs := NewBookService(nil)
+
+	data := []struct {
+		name     string
+		id       int
+		expected bool
+	}{
+		{
+			name:     "valid id",
+			id:       1,
+			expected: true,
+		},
+		{
+			name:     "invalid id - negative id",
+			id:       -1,
+			expected: false,
+		},
+		{
+			name:     "invalid id - zero id",
+			id:       0,
+			expected: false,
+		},
+	}
+
+	for _, d := range data {
+		t.Run(d.name, func(t *testing.T) {
+			assert.Equal(t, d.expected, bs.validateID(d.id))
+		})
+	}
 }
 
 func TestValidateAuthor(t *testing.T) {
-	// TODO: Implement
+	bs := NewBookService(nil)
+
+	data := []struct {
+		name     string
+		author   string
+		expected bool
+	}{
+		{
+			name:     "valid author",
+			author:   "J.R.R. Tolkien",
+			expected: true,
+		},
+		{
+			name:     "empty author",
+			author:   "",
+			expected: false,
+		},
+	}
+
+	for _, d := range data {
+		t.Run(d.name, func(t *testing.T) {
+			assert.Equal(t, d.expected, bs.validateAuthor(d.author))
+		})
+	}
 }
 
 func TestValidateTitle(t *testing.T) {
-	// TODO: Implement
+	bs := NewBookService(nil)
+
+	data := []struct {
+		name     string
+		title    string
+		expected bool
+	}{
+		{
+			name:     "valid title",
+			title:    "The Hobbit",
+			expected: true,
+		},
+		{
+			name:     "empty title",
+			title:    "",
+			expected: false,
+		},
+	}
+
+	for _, d := range data {
+		t.Run(d.name, func(t *testing.T) {
+			assert.Equal(t, d.expected, bs.validateTitle(d.title))
+		})
+	}
 }
