6cabd1ffa8874e2930d96d2a6473595ab6938547
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-08-10 19:14:19 +0200

    Prepare testing functions for book service

--- internal/services/book_service_test.go
+++ internal/services/book_service_test.go
@@ -1,1 +1,35 @@
 package services
+
+import "testing"
+
+func TestGetBooks(t *testing.T) {
+	// TODO: Implement
+}
+
+func TestGetBook(t *testing.T) {
+	// TODO: Implement
+}
+
+func TestAddBook(t *testing.T) {
+	// TODO: Implement
+}
+
+func TestUpdateBook(t *testing.T) {
+	// TODO: Implement
+}
+
+func TestDeleteBook(t *testing.T) {
+	// TODO: Implement
+}
+
+func TestValidateID(t *testing.T) {
+	// TODO: Implement
+}
+
+func TestValidateAuthor(t *testing.T) {
+	// TODO: Implement
+}
+
+func TestValidateTitle(t *testing.T) {
+	// TODO: Implement
+}
