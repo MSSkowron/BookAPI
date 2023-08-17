7bcd04d0f5134273ea750a08d29ffabb33420e0d
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-08-17 17:28:47 +0200

    Add database schema to readme & Set varchar fields length to 255

---
+++ dbscripts/init003.sql
@@ -0,0 +1,17 @@
+alter table users
+alter column email type varchar(255);
+
+alter table users
+alter column password type varchar(255);
+
+alter table users
+alter column first_name type varchar(255);
+
+alter table users
+alter column last_name type varchar(255);
+
+alter table books
+alter column author type varchar(255);
+
+alter table books
+alter column title type varchar(255);
