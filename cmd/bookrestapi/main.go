e4064bc21fe45a3cb317b4e1b292d3404dfd4c26
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-08-21 08:18:17 +0200

    Modify error handling in main function

--- cmd/bookrestapi/main.go
+++ cmd/bookrestapi/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"github.com/MSSkowron/BookRESTAPI/internal/app"
+	"github.com/MSSkowron/BookRESTAPI/pkg/logger"
 )
 
 func main() {
 	if err := app.Run(); err != nil {
-		panic(err)
+		logger.Errorln(err)
 	}
 }
